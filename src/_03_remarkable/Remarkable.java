8adbfea3b3ae83df6ece8873cfbe8d219e992481
josephredivosems <56978443+josephredivosems@users.noreply.github.com>
2019-11-13 16:22:41 -0800

    Finished

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,11 +8,25 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+			String Pablo = "Good coder";
+			String pabllav = "GOOD UHHH";
+			String Peeblo = "YEETER";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String ace =	JOptionPane.showInputDialog("What is your name?");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+	if (ace.equals("Pablo")) {
+	JOptionPane.showMessageDialog(null, "You are a " + Pablo);	
 	}
+	else {
+	if (ace.equals("pabllav")) {
+	JOptionPane.showMessageDialog(null, "You are a "+ pabllav);
+	}
+	else {
+		if (ace.equals("Peeblo")) {
+		JOptionPane.showMessageDialog(null, "You are a " + Peeblo);
+		}
+	}
+	}
+}
 }
 
--- src/_04_secret_message_box/SecretMessageBox.java
+++ src/_04_secret_message_box/SecretMessageBox.java
@@ -15,18 +15,116 @@ import javax.swing.JOptionPane;
  */
 
 public class SecretMessageBox {
+public static void main(String[] args) {
+	
 
 	// 0. Make a main method and put steps 1-5 inside it
 	
 	// 1. Set a password in a String variable
-	
+	String password = "12345";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	String secret = JOptionPane.showInputDialog("I want a secret message from you!");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
-
+	String attempt =	JOptionPane.showInputDialog("Guess the Password and I will show you the previous person's message!!");
 	// 4. If their guess matches the password, show them the secret message
-
+	if(attempt.equals("12345")) {
+		JOptionPane.showMessageDialog(null, "The previous person's message was " + secret);
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "INTRUDER!!");
+	}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
